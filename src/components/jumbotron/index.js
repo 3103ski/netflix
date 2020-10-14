e21c086083e1cec69cd4785a0c7dc5fdd29efec1
3103ski <bjastski@gmail.com>
2020-10-13 22:22:45 -0700

    implioments tree shaking for jumbotron component

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image, Title, SubTitle, Item, Pane, Container, Inner } from './styles/jumbotron';
 
-export default function Jumbtron({ children, direction = 'row', ...restProps }) {
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
 	return (
 		<Item {...restProps}>
 			<Inner direction={direction}>{children}</Inner>
@@ -9,18 +9,18 @@ export default function Jumbtron({ children, direction = 'row', ...restProps }) 
 	);
 }
 
-Jumbtron.Container = function JumbotronContainer({ children, ...restProps }) {
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
 	return <Container {...restProps}>{children}</Container>;
 };
-Jumbtron.Pane = function JumbotronPane({ children, ...restProps }) {
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
 	return <Pane {...restProps}>{children}</Pane>;
 };
-Jumbtron.Title = function JumbotronTitle({ children, ...restProps }) {
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
 	return <Title {...restProps}>{children}</Title>;
 };
-Jumbtron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
 	return <SubTitle {...restProps}>{children}</SubTitle>;
 };
-Jumbtron.Image = function JumbotronSImage({ children, ...restProps }) {
+Jumbotron.Image = function JumbotronSImage({ children, ...restProps }) {
 	return <Image {...restProps} />;
 };
