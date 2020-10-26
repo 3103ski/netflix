dc7ebbb5ecfa714552d343e9e1b7b835bf5dc1e0
3103ski <bjastski@gmail.com>
2020-10-25 18:32:16 -0700

    adds header component with header feature

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,4 @@
 export { default as Footer } from './footer';
 export { default as Faqs } from './faqs';
 export { default as Jumbortron } from './jumbotron';
+export { default as HeaderContainer } from './header';
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
-import { Faqs, Jumbortron, Footer } from '../containers';
+import { OptForm, Feature } from '../components';
+import { Faqs, Jumbortron, Footer, HeaderContainer } from '../containers';
 
 export default function Home() {
 	return (
 		<>
+			<HeaderContainer>
+				<Feature>
+					<Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+					<Feature.SubTitle>Watch anywhere. Cancel at anytime.</Feature.SubTitle>
+					<OptForm>
+						<OptForm.Input placeholder='Enter email address'></OptForm.Input>
+						<OptForm.Button>Try It Now</OptForm.Button>
+						<OptForm.Break></OptForm.Break>
+						<OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+					</OptForm>
+				</Feature>
+			</HeaderContainer>
 			<Jumbortron />
 			<Faqs />
 			<Footer />
