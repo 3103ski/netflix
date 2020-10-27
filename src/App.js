ad017b027ab44db60041a2c02447de9691c8a05f
3103ski <bjastski@gmail.com>
2020-10-26 20:43:22 -0700

    adds auth listener and protected routed components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import * as ROUTES from './constants/routes';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Home, Browse, Signin, Signup } from './pages/';
+import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 export default function App() {
+	const { user } = useAuthListener();
+	// const user = null;
+	console.log(user);
 	return (
 		<Router>
-			<Route exact path={ROUTES.SIGN_UP}>
-				<Signup />
-			</Route>
-			<Route exact path={ROUTES.SIGN_IN}>
+			<IsUserRedirect loggedInPath={ROUTES.BROWSE} user={user} path={ROUTES.SIGN_IN}>
 				<Signin />
-			</Route>
-			<Route exact path={ROUTES.BROWSE}>
+			</IsUserRedirect>
+
+			<IsUserRedirect loggedInPath={ROUTES.BROWSE} user={user} path={ROUTES.SIGN_UP}>
+				<Signup />
+			</IsUserRedirect>
+
+			<ProtectedRoute user={user} path={ROUTES.BROWSE}>
 				<Browse />
-			</Route>
-			<Route exact path={ROUTES.HOME}>
+			</ProtectedRoute>
+
+			<IsUserRedirect exact loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} user={user}>
 				<Home />
-			</Route>
+			</IsUserRedirect>
 		</Router>
 	);
 }
